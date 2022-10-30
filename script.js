60f6e8e5d0a07a284ee5623f7eca405a90cd8f3a
GoTierGod <ivan.gunlove_10_@hotmail.com>
2022-10-30 05:32:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// Cuando el DOM esta listo, se ejecuta inmediatamente esta function (cuando el contenido ha cargado completamente)
 window.addEventListener('load', () => {
+    // Variables de los inputs
     const name = document.getElementById('name');
     const salary = document.getElementById('salary');
     const months = document.getElementById('months');
     
+    // Variables de los elementos donde se mostraran los datos
     const disName = document.getElementById('dis-name');
     const disTotal = document.getElementById('dis-total');
     const employee = document.getElementById('employee');
 
+    // 1 - AJAX
     async function getData() {
         let response = await fetch('user.json')
         return await response.json();
@@ -16,6 +20,8 @@ window.addEventListener('load', () => {
         getData().then(data => employee.innerText = `Name: ${data['name']}\n Age: ${data.age}`);
     }
 
+    // 2 - DOM Manipulation
+    // Evento y funcion para mostrar el nombre
     name.addEventListener('keyup', () => {
         disName.innerText = name.value;
         disEmployee();
@@ -26,6 +32,7 @@ window.addEventListener('load', () => {
         disEmployee();
     }
 
+    // EVENTOS para el mostrar el calculo del bonus
     salary.addEventListener('keyup', updateTotal);
     months.addEventListener('keyup', updateTotal);
 });
